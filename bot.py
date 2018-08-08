303eb95ef3c762e4e5680a9dd382a4409fedb46a
Luke Rindels <lukerindels983@gmail.com>
2018-08-08 11:36:19 -0600

    refactoring and bug fixes

--- bot.py
+++ bot.py
@@ -13,26 +13,30 @@ import time
 import os
 
 
-factor = 8
-reset = 60
+factor = 8 # adjusts obstacle width over time, higher value = slower scaling
+reset = 40 # if count_r reaches this value, game resets
 
 # Screen Coordinates
-class Coordinates:
+# These vary based on screen
+class Coords:
     # restart button
     replay_button = (720, 360)
     # top right of dinosaur
-    dino = (487 * 2, 371 * 2)
+    dino = (487, 371)
     # top left of obstacle box
     obs_orig = (490, 370)
-    # bottom right of obstacle box
-    obs_wh = (90, 20)
-    # gameplay area
+    # initial width and height of obstacle box
+    obs_width = 90
+    obs_height = 20
+    # top left of gameplay area
     game_orig = (400, 262)
-    game_wh = (1030, 432)
+    # width and height of gameplay area
+    game_width = 1030 
+    game_height = 432 
     
 # Clicks restart button
-def restartGame():
-    pyautogui.click(Coordinates.replay_button)
+def click(coord):
+    pyautogui.click(coord)
 
 # Makes dinosaur jump
 def jump():
@@ -43,47 +47,47 @@ def jump():
 
 # Grabs obstacle box, converts to grayscale, returns sum of pixels
 def imageGrab(shift = 0):
-    screenstr = 'screencapture -R' + str(Coordinates.obs_orig[0]) + ',' + str(Coordinates.obs_orig[1]) + ',' + str(Coordinates.obs_wh[0] + shift) + ',' + str(Coordinates.obs_wh[1]) + ' grab.png' 
+    screenstr = 'screencapture -R' + str(Coords.obs_orig[0]) + ',' + str(Coords.obs_orig[1]) + ',' + str(Coords.obs_width + shift) + ',' + str(Coords.obs_height) + ' grab.png' 
     os.system(screenstr) 
     image = Image.open("grab.png")  
     grayscale = ImageOps.grayscale(image)
     a = array(grayscale.getcolors())
     time.sleep(0.02) 
     # print(a)
-    # print (a.sum())
     return a
 
 # Game loop
 print("--- Dino Bot ---")
-print("X Origin =", Coordinates.obs_orig[0])
-print("X Width =", Coordinates.obs_wh[0])
+print("X Origin =", Coords.obs_orig[0])
+print("X Width =", Coords.obs_width)
 print("Speed Factor =", factor)
 games = 1
 while True:
     # two restarts to get window in focus
-    restartGame()
-    restartGame()
-    time.sleep(1)
-    # jump and initialize obstacle box
-    jump()
+    click(Coords.replay_button)
+    click(Coords.replay_button)
+    # moves mouse out of the way
+    click(Coords.game_orig)
+
     # restart counter
-    x = 1
+    count_r = 0
     # progression counter
-    y = 1
+    count_p = 0
 
     print("Games =", games)
     # check for obstacles, jump if obstacle is in obstacle box
     while True:
         # reset if game over
-        if x == reset: 
-            screenstr = 'screencapture -R' + str(Coordinates.game_orig[0]) + ',' + str(Coordinates.game_orig[1]) + ',' + str(Coordinates.game_wh[0]) + ',' + str(Coordinates.game_wh[1]) + ' game' + str(games) + '.png'
+        if count_r == reset: 
+            screenstr = 'screencapture -R' + str(Coords.game_orig[0]) + ',' + str(Coords.game_orig[1]) + ',' + str(Coords.game_width) + ',' + str(Coords.game_height) + ' game' + str(games) + '.png'
             os.system(screenstr) 
             games += 1
             break;
-        if imageGrab((int(round(y / factor))))[0][1] != 247: 
-            jump()
-            x = 0
-        x += 1 
-        y += 1
-        #print("x =", x)
-        #print("y =", y) 
+        if imageGrab((int(round(count_p / factor))))[0][1] != 247: 
+            jump()    
+            count_r = 0 
+
+        count_r += 1 
+        count_p += 1
+        print("reset counter =", count_r)
+        print("progression counter =", count_p)
